bfd5f0a6d7c42c9b84d463113cebc16f460f23ea
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 20:32:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  i = 0 #placeholder
+  names_collection = []
+  while i < array.length #remember not to use size. always length for this.
+    names_collection << yield(array[i]) #could use push but this looks nicer
+    i += 1 #counter each time goes up
+  end
+  names_collection #return to make sure the method works
+end
 
-
+#make sure you check spelling
